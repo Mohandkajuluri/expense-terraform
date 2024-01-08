ec44d232e033fb8e89b755ef02a233dfb785953e
Mohandkajuluri <md.kajuluri@gmail.com>
2024-01-08 15:11:04 -0500

    expense DNS records

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z07877831F6XHEVGE7HRM"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "backend" {
   ami = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -14,10 +22,26 @@ resource "aws_instance" "backend" {
   }
 }
 
+resource "aws_route53_record" "backend" {
+  zone_id = "Z07877831F6XHEVGE7HRM"
+  name    = "backend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mysql" {
   ami = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
   tags = {
     Name = "mysql"
   }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z07877831F6XHEVGE7HRM"
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
 }
